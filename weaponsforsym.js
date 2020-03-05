ec9779718d7bf93da31a24ae62c96b9875ce3e70
Russo <60620815+irussao@users.noreply.github.com>
2020-03-05 15:10:45 -0300

    Add files via upload

---
+++ weaponsforsym.js
@@ -0,0 +1,365 @@
+SymWeapons = {
+  "assault_rifles": {
+    "weaponMult": 2.13,
+    "ak-12": {
+      "name": "AK-12"
+    },
+    "scar-h": {
+      "name": "SCAR-H"
+    },
+    "m416": {
+      "name": "M416"
+    },
+    "sar-21": {
+      "name": "SAR-21"
+    },
+    "aek-971": {
+      "name": "AEK-971"
+    },
+    "famas": {
+      "name": "FAMAS"
+    },
+    "aug-a3": {
+      "name": "AUG A3"
+    },
+    "m16a4": {
+      "name": "M16A4"
+    },
+    "cz-805": {
+      "name": "CZ-805"
+    },
+    "f2000": {
+      "name": "F2000"
+    },
+    "ace-23": {
+      "name": "ACE 23"
+    },
+    "qbz-95-1": {
+      "name": "QBZ-95-1"
+    },
+    "bulldog": {
+      "name": "Bulldog"
+    },
+    "ar160": {
+      "name": "AR160"
+    },
+    "l85a2": {
+      "name": "L85A2"
+    },
+    "an-94": {
+      "name": "AN-94"
+    }
+  },
+  "carbines": {
+    "weaponMult": 2.13,
+    "ak-5c": {
+      "name": "AK 5C"
+    },
+    "acw-r": {
+      "name": "ACW-R"
+    },
+    "sg553": {
+      "name": "SG553"
+    },
+    "aku-12": {
+      "name": "AKU-12"
+    },
+    "a-91": {
+      "name": "A-91"
+    },
+    "ace-52-cqb": {
+      "name": "ACE 52 CQB"
+    },
+    "g36c": {
+      "name": "G36C"
+    },
+    "m4": {
+      "name": "M4"
+    },
+    "ace-21-cqb": {
+      "name": "ACE 21 CQB"
+    },
+    "type-95b-1": {
+      "name": "Type-95B-1"
+    },
+    "mtar-21": {
+      "name": "MTAR-21"
+    },
+    "phantom": {
+      "name": "Phantom"
+    },
+    "groza-1": {
+      "name": "Groza-1"
+    }
+  },
+  "dmrs": {
+    "weaponMult": 2,
+    "ace-53-sv": {
+      "name": "ACE 53 SV"
+    },
+    "m39-emr": {
+      "name": "M39 EMR"
+    },
+    "qbu-88": {
+      "name": "QBU-88"
+    },
+    "mk11-mod-0": {
+      "name": "MK11 MOD 0"
+    },
+    "scar-h-sv": {
+      "name": "SCAR-H SV"
+    },
+    "sks": {
+      "name": "SKS"
+    },
+    "rfb": {
+      "name": "RFB"
+    },
+    "svd-12": {
+      "name": "SVD-12"
+    }
+  },
+  "handguns": {
+    "weaponMult": 2.13,
+    "p226": {
+      "name": "P226"
+    },
+    "m412-rex": {
+      "name": "MP412 Rex"
+    },
+    "compact-45": {
+      "name": "Compact 45"
+    },
+    "m1911": {
+      "name": "M1911"
+    },
+    "44-magnum": {
+      "name": "44 Magnum"
+    },
+    "93r": {
+      "name": "93R"
+    },
+    "mp443": {
+      "name": "MP443"
+    },
+    "g18": {
+      "name": "G18"
+    },
+    "cz-75": {
+      "name": "CZ-75"
+    },
+    "qsz-92": {
+      "name": "QSZ-92"
+    },
+    "m9": {
+      "name": "M9"
+    },
+    "fn57": {
+      "name": "FN57"
+    },
+    "sw40": {
+      "name": "SW40"
+    },
+    "deagle-44": {
+      "name": "Deagle 44"
+    },
+    "unica-6": {
+      "name": "Unica 6"
+    },
+    "mare-s-leg": {
+      "name": "Mares Leg"
+    }
+  },
+  "lmgs": {
+    "weaponMult": 2.13,
+    "mg4": {
+      "name": "MG4"
+    },
+    "lsat": {
+      "name": "LSAT"
+    },
+    "m240b": {
+      "name": "M240B"
+    },
+    "qbb-95-1": {
+      "name": "QBB-95-1"
+    },
+    "type-88-lmg": {
+      "name": "Type-88 LMG"
+    },
+    "u-100-mk5": {
+      "name": "U-100 MK5"
+    },
+    "rpk-12": {
+      "name": "RPK-12"
+    },
+    "pkp-pecheneg": {
+      "name": "PKP Pecheneg"
+    },
+    "m249": {
+      "name": "M249"
+    },
+    "aws": {
+      "name": "AWS"
+    },
+    "m60-e4": {
+      "name": "M60E4"
+    },
+    "rpk": {
+      "name": "RPK-74M"
+    },
+    "l86a2": {
+      "name": "L86A2"
+    },
+    "id-p-xp6-iname-m60ult": {
+      "name": "M60ULT"
+    }
+  },
+  "sniper_rifles": {
+    "weaponMult": 2.35,
+    "cs-lr4": {
+      "name": "CS-LR4"
+    },
+    "m82a3-mid": {
+      "name": "M82A3"
+    },
+    "sv-98": {
+      "name": "SV98"
+    },
+    "amr-2": {
+      "name": "AMR-2"
+    },
+    "m40a5": {
+      "name": "M40A5"
+    },
+    "m82a3-cqb": {
+      "name": "M82A3 CQB"
+    },
+    "338-recon": {
+      "name": "338-Recon"
+    },
+    "scout-elite": {
+      "name": "Scout Elite"
+    },
+    "m82a3": {
+      "name": "M82A3"
+    },
+    "jng-90": {
+      "name": "JNG-90"
+    },
+    "m98b": {
+      "name": "M98B"
+    },
+    "fy-js": {
+      "name": "FY-JS"
+    },
+    "amr-2-cqb": {
+      "name": "AMR-2 CQB"
+    },
+    "amr-2-mid": {
+      "name": "AMR-2 MID"
+    },
+    "rorsch-mk-1": {
+      "name": "Railgun"
+    },
+    "srr-61": {
+      "name": "SRR-61"
+    },
+    "gol-magnum": {
+      "name": "GOL Magnum"
+    },
+    "l115": {
+      "name": "L115"
+    },
+    "sr338": {
+      "name": "SR338"
+    },
+    "cs5": {
+      "name": "CS5"
+    }
+  },
+  "pdws": {
+    "weaponMult": 2.13,
+    "mx4": {
+      "name": "MX4"
+    },
+    "pp-2000": {
+      "name": "PP-2000"
+    },
+    "cbj-ms": {
+      "name": "CBJ-MS"
+    },
+    "ump-45": {
+      "name": "UMP-45"
+    },
+    "cz-3a1": {
+      "name": "CZ-3A1"
+    },
+    "p90": {
+      "name": "P90"
+    },
+    "pdw-r": {
+      "name": "PDW-R"
+    },
+    "ump-9": {
+      "name": "UMP-9"
+    },
+    "js2": {
+      "name": "JS2"
+    },
+    "as-val": {
+      "name": "AS VAL"
+    },
+    "mp7": {
+      "name": "MP7"
+    },
+    "sr-2": {
+      "name": "SR-2"
+    },
+    "mpx": {
+      "name": "MPX"
+    },
+    "groza-4": {
+      "name": "Groza-4"
+    }
+  },
+  "shotguns": {
+    "weaponMult": 1,
+    "dao-12": {
+       "name": "dao-12"
+    },
+    "dbv-12": {
+       "name": "dbv-12"
+    },
+    "hawk-12g": {
+       "name": "hawk-12g"
+    },
+    "m1014": {
+       "name": "m1014"
+    },
+    "qbs-09": {
+       "name": "qbs-09"
+    },
+    "870-mcs": {
+       "name": "870-mcs"
+    },
+    "saiga-12k": {
+       "name": "saiga-12k"
+    },
+    "shorty-12g": {
+       "name": "shorty-12g"
+    },
+    "spas-12": {
+       "name": "spas-12"
+    },
+    "uts-15": {
+       "name": "uts-15"
+    },
+    "usas-12": {
+       "name": "usas-12"
+    },
+    "usas-12-flir": {
+       "name": "usas-12-flir"
+    }
+  }
+} 
