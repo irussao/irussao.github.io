cd4758d9e665252a47ed70341673317c7778c7fb
Russo <60620815+irussao@users.noreply.github.com>
2020-03-19 20:51:31 -0300

    Update bblogupdate.js

--- bblogupdate.js
+++ bblogupdate.js
@@ -1,3 +1,4 @@
 bblog = {
-  "version":"5.3.2"
+  "version":"5.3.2",
+  "video":"https://www.youtube.com/embed/nAjdVGjTcSM?autoplay=1&loop=1&mute=1&playlist=nAjdVGjTcSM&nocache=1579365119246&hd=1"
 }
