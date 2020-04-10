0115803710bf230e60263d265f0077ec7317e0c6
Russo <60620815+irussao@users.noreply.github.com>
2020-04-10 13:43:11 -0300

    Update bblogupdate.js

--- bblogupdate.js
+++ bblogupdate.js
@@ -1,4 +1,5 @@
 bblog = {
   "version":"5.3.1",
-  "video":"https://www.youtube.com/embed/nAjdVGjTcSM?autoplay=1&loop=1&mute=1&playlist=nAjdVGjTcSM&nocache=1579365119246&hd=1"
+	"video":"https://www.youtube.com/embed/nAjdVGjTcSM?autoplay=1&loop=1&mute=1&playlist=nAjdVGjTcSM&nocache=1579365119246&hd=1",
+	"allowadmb": ["350573751", "806262072", "241511593", "992384984", "353041355", "400990007", "1053499150", "1389309154", "225941700", "777484340", "253567412", "1950889960", "1684551890"]
 }
