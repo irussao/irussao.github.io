f8b154c42bfba020cfbacf288b188dfaea62b1c4
Russo <60620815+irussao@users.noreply.github.com>
2020-04-10 14:08:15 -0300

    Update cheatscore.js

--- bbl/cheatscore.js
+++ bbl/cheatscore.js
@@ -1,5 +1,4 @@
 var loadcheatscore = false;
-var allowadmb = ["350573751", "806262072", "241511593", "992384984", "353041355", "400990007", "1053499150", "1389309154", "225941700", "777484340", "253567412", "1950889960", "1684551890"];
 if (loadcheatscore) {
 	document.getElementById("cheatscore").innerHTML = "True, Add HTML input button here.";
 }
